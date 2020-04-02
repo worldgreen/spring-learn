f2781238512fd4cbb0e8d72bdd3bb5db49218b1d
wanghongen <wanghongen@yidian-inc.com>
2020-04-02 21:08:31 +0800

    add: tx

--- src/main/resources/bash/d402.sh
+++ src/main/resources/bash/d402.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 start=${1};
 end=`date -d "${start} + 3 days" +%F`;
-while [ "${start}" -lt "${end}" ]; do
+while [ `${start} +%F` < `${end} +%F` ]; do
 echo -n "${start}";
 start=`date -d "${start} + 1 days" +%F`;
 done;
